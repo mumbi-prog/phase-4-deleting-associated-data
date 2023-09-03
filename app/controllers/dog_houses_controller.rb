1c07d7d7dd04caea367e2b3162d6d1d42c077d5f
Sylvia Mumbi <mumbi099@gmail.com>
2023-09-03 15:15:42 +0300

    Done lab

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,9 +1,19 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def index
+    render json: DogHouse.all, status: :ok
+  end
+
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house, include: :reviews
+  end
+
+  def destroy
+    dog_house = DogHouse.find(params[:id])
+    dog_house.destroy
+    head :no_content
   end
 
   private
